2fbf622d01de7ceacc3afbe10216be5547de5bb3
al-shark <sharkasmail@gmail.com>
2017-02-27 22:02:58 +0200

    Stage 2

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,9 +1,17 @@
 package com.javarush.task.task21.task2113;
 
+import java.util.List;
+
 /**
  * Created by sharka on 27.02.2017.
  */
 public class Hippodrome {
+    private List<Horse> horses;
+
+    public List<Horse> getHorses() {
+        return horses;
+    }
+
     public static void main(String[] args) {
 
     }
