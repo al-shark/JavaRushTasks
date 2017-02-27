fe2dc847678820525b86e8bd00b5e0588c0138f6
al-shark <sharkasmail@gmail.com>
2017-02-27 13:34:21 +0200

    Solved

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2105/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2105/Solution.java
@@ -15,10 +15,20 @@ public class Solution {
     }
 
     public boolean equals(Object o) {
-        if (!(o instanceof Solution))
-            return false;
-        Solution n = (Solution) o;
-        return n.first.equals(first) && n.last.equals(last);
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof Solution)) return false;
+
+        Solution solution = (Solution) o;
+
+        if (first != null ? !first.equals(solution.first) : solution.first != null) return false;
+        return last != null ? last.equals(solution.last) : solution.last == null;
+    }
+
+    public int hashCode() {
+        int result = first != null ? first.hashCode() : 0;
+        result = 31 * result + (last != null ? last.hashCode() : 0);
+        return result;
     }
 
     public static void main(String[] args) {
