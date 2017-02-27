5e7ed0a153a1e35f93e926ac1ba0cec018059db7
al-shark <sharkasmail@gmail.com>
2017-02-27 22:20:55 +0200

    Stage 6

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task21.task2113;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,6 +8,7 @@ import java.util.List;
  */
 public class Hippodrome {
     private List<Horse> horses;
+    public static Hippodrome game;
 
     public Hippodrome(List<Horse> horses) {
         this.horses = horses;
@@ -17,6 +19,11 @@ public class Hippodrome {
     }
 
     public static void main(String[] args) {
+        List<Horse> h = new ArrayList<>();
+        h.add(new Horse("Horse1",3,0));
+        h.add(new Horse("Horse2",3,0));
+        h.add(new Horse("Horse3",3,0));
+        game = new Hippodrome(h);
 
     }
 }
