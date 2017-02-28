28ba8cad09e8364afacc8aac9217128618d00eb7
al-shark <sharkasmail@gmail.com>
2017-02-28 11:31:24 +0200

    Solved

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2203/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task22.task2203;
+
+/* 
+Между табуляциями
+*/
+public class Solution {
+    public static String getPartOfString(String string) throws TooShortStringException {
+        if (string==null) throw new TooShortStringException();
+
+        String[] sResult = string.split("\\t",3);
+
+        if (sResult.length<3) throw new TooShortStringException();
+
+        return sResult[1];
+    }
+
+    public static class TooShortStringException extends Exception {
+    }
+
+    public static void main(String[] args) throws TooShortStringException {
+        System.out.println(getPartOfString("\tJavaRush - лучший сервис \tобучения Java\t."));
+    }
+}
