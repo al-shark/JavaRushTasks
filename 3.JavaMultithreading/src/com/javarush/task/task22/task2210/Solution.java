77b2587b60716bec10ac16c0c680a469ba700495
al-shark <sharkasmail@gmail.com>
2017-03-05 00:16:41 +0200

    Solved 2209

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2210/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task22.task2210;
+
+import java.util.StringTokenizer;
+
+/*
+StringTokenizer
+*/
+public class Solution {
+    public static void main(String[] args) {
+        String[] t = getTokens("sfd. werwer .wrwer . wer.werwerwe.werwer.werwer.wer.wer.we.","\\.");
+        for (String x : t) System.out.println(x);
+    }
+
+    public static String [] getTokens(String query, String delimiter) {
+        StringTokenizer tokens = new StringTokenizer(query,delimiter);
+        String[] result = new String[tokens.countTokens()];
+        for (int i=0; i<result.length; i++) result[i]=tokens.nextToken();
+        return result;
+    }
+}
