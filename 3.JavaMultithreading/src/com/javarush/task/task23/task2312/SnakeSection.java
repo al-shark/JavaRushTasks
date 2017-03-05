54971ee89921c9d96a8b47a473e08b51d93a8758
al-shark <sharkasmail@gmail.com>
2017-03-05 23:05:25 +0200

    Solved 2312-06

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
@@ -4,4 +4,19 @@ package com.javarush.task.task23.task2312;
  * Created by sharka on 05.03.2017.
  */
 public class SnakeSection {
+    private int x,y;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public SnakeSection(int x, int y) {
+
+        this.x = x;
+        this.y = y;
+    }
 }
