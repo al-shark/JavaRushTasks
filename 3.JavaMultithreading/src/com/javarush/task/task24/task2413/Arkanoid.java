5e08027ab7d9a88e59a946954379986415f179f7
al-shark <sharkasmail@gmail.com>
2017-03-08 01:10:21 +0200

    Solved 2413-01

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task24.task2413;
+
+/**
+ * Created by sharka on 08.03.2017.
+ */
+public class Arkanoid {
+
+    public static void main (String[] args) {
+
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task24.task2413;
+
+/**
+ * Created by sharka on 08.03.2017.
+ */
+public class Ball {
+}
