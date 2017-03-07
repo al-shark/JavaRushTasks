b41c59cea84c476cdc249ed336e092aed3c96ed7
al-shark <sharkasmail@gmail.com>
2017-03-08 01:15:23 +0200

    Solved 2413-04

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -1,10 +1,39 @@
 package com.javarush.task.task24.task2413;
 
+import java.util.List;
+
 /**
  * Created by sharka on 08.03.2017.
  */
 public class Arkanoid {
     private int width, height;
+    private Ball ball;
+    private Stand stand;
+    private List<Brick> bricks;
+
+    public Ball getBall() {
+        return ball;
+    }
+
+    public void setBall(Ball ball) {
+        this.ball = ball;
+    }
+
+    public Stand getStand() {
+        return stand;
+    }
+
+    public void setStand(Stand stand) {
+        this.stand = stand;
+    }
+
+    public List<Brick> getBricks() {
+        return bricks;
+    }
+
+    public void setBricks(List<Brick> bricks) {
+        this.bricks = bricks;
+    }
 
     public Arkanoid(int width, int height) {
         this.width = width;
