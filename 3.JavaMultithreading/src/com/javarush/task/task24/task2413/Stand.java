f9328e3f03fc5d3d4df43395c54758a78113b950
al-shark <sharkasmail@gmail.com>
2017-03-08 13:30:40 +0200

    Solved 2413-16

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -4,8 +4,21 @@ package com.javarush.task.task24.task2413;
  * Created by sharka on 08.03.2017.
  */
 public class Stand extends BaseObject {
-    public Stand(double x, double y, double radius) {
-        super(x, y, radius);
+    private double speed;
+    private double direction;
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public Stand(double x, double y) {
+        super(x, y, 3);
+        speed = 1;
+        direction = 0;
     }
 
     @Override
