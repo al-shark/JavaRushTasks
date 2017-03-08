e2f240b9bc996e98f2f78ff683f4360ee9a93d20
al-shark <sharkasmail@gmail.com>
2017-03-09 00:52:22 +0200

    Solved 2409

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2409/Item.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task24.task2409;
+
+/**
+ * Created by sharka on 09.03.2017.
+ */
+public interface Item {
+
+    int getId();
+
+    double getPrice();
+
+    String getTM();
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2409/Jeans.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task24.task2409;
+
+/**
+ * Created by sharka on 09.03.2017.
+ */
+public interface Jeans extends Item {
+
+    int getLength();
+
+    int getSize();
+}
