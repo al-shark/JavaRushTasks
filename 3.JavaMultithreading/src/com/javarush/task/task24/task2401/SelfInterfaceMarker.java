98fdaf3e3b1f310945d2a83b302f7db7f3b8c5bf
al-shark <sharkasmail@gmail.com>
2017-03-08 15:20:54 +0200

    Solved 2401

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarker.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task24.task2401;
+
+/**
+ * Created by sharka on 08.03.2017.
+ */
+public interface SelfInterfaceMarker {
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task24.task2401;
+
+/**
+ * Created by sharka on 08.03.2017.
+ */
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker {
+    public void Test1() {}
+
+    public void Test2() {}
+
+}
