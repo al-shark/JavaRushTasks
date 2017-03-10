86da7a51e1769ca1748ac69ca3e35b758f08196e
al-shark <sharkasmail@gmail.com>
2017-03-10 23:58:00 +0200

    Solved 2502

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2502/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task25.task2502;
+
+import java.util.*;
+
+/* 
+Машину на СТО не повезем!
+*/
+public class Solution {
+    public static enum Wheel {
+        FRONT_LEFT,
+        FRONT_RIGHT,
+        BACK_LEFT,
+        BACK_RIGHT
+    }
+
+    public static class Car {
+        protected List<Wheel> wheels;
+
+        public Car() {
+            String[] getWeels = loadWheelNamesFromDB();
+            if (getWeels.length!=4) throw new IllegalArgumentException();
+
+            wheels = new ArrayList<>();
+            for (String x : getWeels) {
+                wheels.add(Wheel.valueOf(x));
+            }
+
+            //init wheels here
+        }
+
+        protected String[] loadWheelNamesFromDB() {
+            //this method returns mock data
+            return new String[]{"FRONT_LEFT", "FRONT_RIGHT", "BACK_LEFT", "BACK_RIGHT"};
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
