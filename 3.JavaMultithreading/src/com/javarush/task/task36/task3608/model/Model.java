aa5e370e2148b5a467e5f2123dc6a5f736756e73
al-shark <sharkasmail@gmail.com>
2017-03-10 02:35:15 +0200

    Solved 3608-01

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/Model.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task36.task3608.model;
+
+/**
+ * Created by sharka on 10.03.2017.
+ */
+public interface Model {
+    ModelData getModelData();
+
+    void loadUsers();
+}
