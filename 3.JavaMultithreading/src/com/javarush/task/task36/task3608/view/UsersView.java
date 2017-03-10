8de948388901525bfbaea083cd4cebe2f4e3bbd9
al-shark <sharkasmail@gmail.com>
2017-03-10 09:17:44 +0200

    Solved 3608-03

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/UsersView.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task36.task3608.view;
+
+import com.javarush.task.task36.task3608.bean.User;
+import com.javarush.task.task36.task3608.controller.Controller;
+import com.javarush.task.task36.task3608.model.ModelData;
+
+/**
+ * Created by sharka on 10.03.2017.
+ */
+public class UsersView implements View {
+    private Controller controller;
+
+    @Override
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    public void refresh(ModelData modelData) {
+        System.out.println("All users:");
+        for (User x : modelData.getUsers()) {
+            System.out.println("\t"+x);
+        }
+        System.out.println("==================================================");
+    }
+
+    public void fireEventShowAllUsers() {
+        controller.onShowAllUsers();
+    }
+}
