4c7e2ab11b03b0644d8d8e9b38e41faf329f30e2
al-shark <sharkasmail@gmail.com>
2017-03-14 15:30:32 +0200

    Solved 2512

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2512/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task25.task2512;
+
+/* 
+Живем своим умом
+*/
+public class Solution implements Thread.UncaughtExceptionHandler {
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        t.interrupt();
+        printStack(e);
+    }
+
+    public void printStack(Throwable e) {
+        if (e.getCause()!=null) printStack(e.getCause());
+        System.out.println(e);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Throwable e = new Exception("ABC", new RuntimeException("DEF", new IllegalAccessException("GHI")));
+        new Solution().uncaughtException(Thread.currentThread(), e);
+    }
+}
