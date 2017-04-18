9cc1301a718dc07b0cc24d708bf3f6bf9032f6d6
al-shark <sharkasmail@gmail.com>
2017-04-18 21:32:54 +0300

    Solved 2909-03

---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Alive.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task29.task2909.human;
+
+/**
+ * Created by sharka on 18.04.2017.
+ */
+public interface Alive {
+    public void live();
+
+}
--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
@@ -9,6 +9,7 @@ public class Student extends Human {
     private String university;
     private Date beginningOfSession;
     private Date endOfSession;
+    private int course;
 
     public Student(String name, int age, double averageGrade) {
         super(name,age);
@@ -20,6 +21,10 @@ public class Student extends Human {
     }
 
     public void learn() {
+    }
+
+    public int getCourse() {
+        return course;
     }
 
     public String getUniversity() {
