ca0d145ab8693c8d72c1f62887354f5150b8173c
al-shark <sharkasmail@gmail.com>
2017-05-15 02:16:07 +0300

    Solved 3008-13

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -1,6 +1,11 @@
 package com.javarush.task.task30.task3008.client;
 
 import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.IOException;
 
 /**
  * Created by sharka on 15.05.2017.
@@ -11,5 +16,35 @@ public class Client {
 
     public class SocketThread extends Thread {
 
+    }
+
+    protected String getServerAddress() {
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort() {
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName() {
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole() {
+        return true;
+    }
+
+    protected SocketThread getSocketThread() {
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text) {
+        try {
+            connection.send(new Message(MessageType.TEXT,text));
+        }
+        catch (IOException e) {
+            System.out.println("Ошибка отправки сообщения.");
+            clientConnected = false;
+        }
     }
 }
