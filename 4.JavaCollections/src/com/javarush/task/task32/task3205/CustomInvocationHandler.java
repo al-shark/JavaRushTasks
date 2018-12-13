00a1a02db8bbb8d3342eaa4936be1601dba1df07
al-shark <sharkasmail@gmail.com>
2018-12-13 16:06:52 +0200

    Solved - 3205

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3205/CustomInvocationHandler.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task32.task3205;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class CustomInvocationHandler implements InvocationHandler {
+    private SomeInterfaceWithMethods someInterfaceWithMethods;
+
+    public CustomInvocationHandler(SomeInterfaceWithMethods intWithMethods) {
+        this.someInterfaceWithMethods = intWithMethods;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println(method.getName()+" in");
+        Object retObj;
+        try {
+            retObj = method.invoke(someInterfaceWithMethods, args);
+        } finally {
+            System.out.println(method.getName()+" out");
+        }
+
+        return retObj;
+    }
+}
