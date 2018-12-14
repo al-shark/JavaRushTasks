885224671249cdce697d091974d59e651e501055
al-shark <sharkasmail@gmail.com>
2018-12-14 22:27:57 +0200

    Mooonlander 10

--- 8.Games/src/com/javarush/games/moonlander/Rocket.java
+++ 8.Games/src/com/javarush/games/moonlander/Rocket.java
@@ -2,11 +2,18 @@ package com.javarush.games.moonlander;
 
 public class Rocket extends GameObject {
     private double speedY = 0;
+    private double speedX = 0;
     private double boost = 0.05;
 
-    public void move() {
-        speedY = speedY + boost;
+    public void move(boolean isUpPressed, boolean isLeftPressed, boolean isRightPressed) {
+        if (isUpPressed) speedY = speedY - boost;
+        else speedY = speedY + boost;
         y = y + speedY;
+
+        boolean isXchanged = false;
+        if (isLeftPressed) {speedX = speedX - boost; isXchanged = true;}
+        if (isRightPressed) {speedX = speedX + boost; isXchanged = true;}
+        if (isXchanged) x = x + speedX;
     }
 
     public Rocket(double x, double y) {
