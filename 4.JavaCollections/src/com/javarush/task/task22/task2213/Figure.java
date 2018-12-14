57a42b78482ee4e89b73530f0ae90839baf02d05
al-shark <sharkasmail@gmail.com>
2018-12-14 02:51:05 +0200

    Solved(Tetris) - 2213-10

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
@@ -5,6 +5,22 @@ public class Figure {
     private int y;
     private int[][] matrix;
 
+    public void left() {}
+
+    public void right() {}
+
+    public void up() {}
+
+    public void down() {}
+
+    public void rotate() {}
+
+    public void downMaximum() {}
+
+    public boolean isCurrentPositionAvailable() {return true;}
+
+    public void landed() {}
+
     public int getX() {
         return x;
     }
