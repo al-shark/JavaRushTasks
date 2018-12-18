5c0c058e37b221992e55056943a9a7715547333e
al-shark <sharkasmail@gmail.com>
2018-12-18 02:14:06 +0200

    Solved (Space) 2515-08

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -5,6 +5,23 @@ public class Canvas {
     private int height;
     private char[][] matrix;
 
+    public void drawMatrix(double x, double y, int[][] matrix, char c) {
+        int xx = (int) Math.round(x);
+        int yy = (int) Math.round(y);
+        for (int i = 0; i < matrix[0].length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (!(matrix[j][i]==0)) setPoint(xx+i,yy+j,c);
+            }
+        }
+    }
+
+    public void setPoint(double x, double y, char c) {
+        int xx = (int) Math.round(x);
+        int yy = (int) Math.round(y);
+
+        if (!((xx<0) || (yy<0) || (xx>=width) || (yy>=height))) matrix[yy][xx] = c;
+    }
+
     public char[][] getMatrix() {
         return matrix;
     }
