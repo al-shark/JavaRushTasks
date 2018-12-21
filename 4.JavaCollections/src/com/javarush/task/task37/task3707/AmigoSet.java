b26cb9d72a5fa6dbe3d635f0ae8f08b8ecbab6a5
al-shark <sharkasmail@gmail.com>
2018-12-21 18:48:30 +0200

    Solved 3707-05

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,11 +1,34 @@
 package com.javarush.task.task37.task3707;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.*;
 
 public class AmigoSet <E> extends AbstractSet implements Cloneable, Serializable, Set {
     private static final Object PRESENT = new Object();
     private transient HashMap<E,Object> map;
+
+    private void writeObject(ObjectOutputStream objectOutputStream) throws IOException{
+        objectOutputStream.defaultWriteObject();
+        objectOutputStream.writeInt(map.size());
+        objectOutputStream.writeInt(HashMapReflectionHelper.callHiddenMethod(map,"capacity"));
+        objectOutputStream.writeFloat(HashMapReflectionHelper.callHiddenMethod(map,"loadFactor"));
+        for (E e: map.keySet()) {
+            objectOutputStream.writeObject(e);
+        }
+    }
+    private void readObject(ObjectInputStream objectInputStream) throws IOException, ClassNotFoundException {
+        objectInputStream.defaultReadObject();
+        int size =  objectInputStream.readInt();
+        int capacity = objectInputStream.readInt();
+        float loadFactor = objectInputStream.readFloat();
+        map = new HashMap(capacity,loadFactor);
+        for (int i = 0; i <size ; i++) {
+            map.put((E) objectInputStream.readObject(),PRESENT);
+            }
+    }
 
     @Override
     public Object clone() throws InternalError {
